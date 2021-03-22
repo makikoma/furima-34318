c8211f0f4a16439042af5977047c54026f419ac8
makikoma <sweetness91116@gmail.com>
2021-03-23 02:11:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :last_name, :first_name, presence: true
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name_kana, :first_name_kana, presence: true
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/}
+  validates :birth_data, presence: true
+
 end
