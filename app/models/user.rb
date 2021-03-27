618209df26a341171bd5ec5a467d92416ea19f36
makikoma <sweetness91116@gmail.com>
2021-03-27 21:34:57 +0900

    修正　バリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
-  validates :last_name, :first_name, presence: true
-  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' }
-  validates :last_name_kana, :first_name_kana, presence: true
-  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' }
-  validates :birth_data, presence: true
+
+  with_options presence: true do
+    validates :nickname, :birth_data
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' }
+  end
 end
