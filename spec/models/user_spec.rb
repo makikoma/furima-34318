bb91b47076c6a09c9ad25d90910a050d9d30cff8
makikoma <sweetness91116@gmail.com>
2021-03-27 21:39:08 +0900

    追加　exampleの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,8 +71,13 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは半角英数字混合以外では登録できない' do
+      it 'passwordは半角数字のみでは登録できない' do
         @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it 'passwordは半角英字のみでは登録できない' do
+        @user.password = 'abcdef'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
       end
