46e4e57dd649c09d97ffb70b94c5d5a891426a59
makikoma <sweetness91116@gmail.com>
2021-04-03 21:02:08 +0900

    Itemモデルの単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,105 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できる時' do
+      it 'image,name,explanation,priceが存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
+      it 'category_idが1でなければ出品できる' do
+        @item.category_id = 2
+        expect(@item).to be_valid
+      end
+      it 'condition_idが1でなければ出品できる' do
+        @item.condition_id = 2
+        expect(@item).to be_valid
+      end
+      it 'postage_idが1でなければ出品できる' do
+        @item.postage_id = 2
+        expect(@item).to be_valid
+      end
+      it 'area_idが1でなければ出品できる' do
+        @item.area_id = 2
+        expect(@item).to be_valid
+      end
+      it 'delivery_day_idが1でなければ出品できる' do
+        @item.delivery_day_id = 2
+        expect(@item).to be_valid
+      end
+      it 'priceが¥300~¥9,999,999の間であれば出品できる' do
+        @item.price = 9_999_999
+        expect(@item).to be_valid
+      end
+      it 'priceは半角数字であれば出品できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できない時' do
+      it 'imageが未選択では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'explanationが空では出品できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'category_idが1では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+      it 'condition_idが1では出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition Select')
+      end
+      it 'postage_idが1では出品できない' do
+        @item.postage_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Postage Select')
+      end
+      it 'area_idが1では出品できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Area Select')
+      end
+      it 'delivery_day_idが1では出品できない' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery day Select')
+      end
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが¥300より低いと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it 'priceが¥9,999,999より高いと出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+      it 'priceは全角数字では出品できない' do
+        @item.price = '１２３４５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Half-width number', 'Price Out of setting range')
+      end
+    end
+  end
 end
