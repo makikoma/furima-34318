aade63882a119abae91574db925947faaa4c8bb6
makikoma <sweetness91116@gmail.com>
2021-04-03 17:16:13 +0900

    Itemモデル バリデーション設定とエラーメッセージ 表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :area
   belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :image, :name, :explanation
+    validates :price, format: { with: /\A[0-9]\d+\z/, message: 'Half-width number' }
+  end
+
+  validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
+
+  validates :category_id, numericality: { other_than: 1, message: 'Select' }
+  validates :condition_id, numericality: { other_than: 1, message: 'Select' }
+  validates :postage_id, numericality: { other_than: 1, message: 'Select' }
+  validates :area_id, numericality: { other_than: 1, message: 'Select' }
+  validates :delivery_day_id, numericality: { other_than: 1, message: 'Select' }
 end
