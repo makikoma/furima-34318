e9df53112cc039526b19b79caeb41724be4e6120
makikoma <sweetness91116@gmail.com>
2021-04-11 10:46:42 +0900

    Formオブジェクトの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,21 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id,
+                :postal_code, :state_id, :city, :address, :building_name,
+                :phone, :purchase_id
+
+  with_options presence: true do
+    validates :user_id, :item_id, :purchas_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :city
+    validates :address
+    validates :phone, format: { with: /\A[0-9]+\z/ }
+  end
+  validates :state_id, numericality: { other_than: 1 }
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, state_id: state_id, city: city, address: address,
+                   building_name: building_name, phone: phone, purchase_id: purchase.id)
+  end
+end
