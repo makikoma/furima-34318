14d1f90292c18325902592da47a77e2818070a47
makikoma <sweetness91116@gmail.com>
2021-04-12 15:12:17 +0900

    親モデルの情報を子モデルのビューで表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,9 @@
 class PurchasesController < ApplicationController
   def index
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
   end
 end
