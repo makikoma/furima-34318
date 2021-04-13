70ba26fe380875fb703f26fb742f273476d85202
makikoma <sweetness91116@gmail.com>
2021-04-13 11:49:58 +0900

    商品購入ページでエラーメッセージ を表示

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,17 +1,15 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id,
-                :postal_code, :state_id, :city, :address, :building_name,
-                :phone
+                :postal_code, :state_id, :city, :address, :building_name, :phone
 
   with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :state_id, numericality: { other_than: 1, message: 'Select' }
+    validates :city, :address
+    validates :phone, format: { with: /\A[0-9]{1,11}\z/, message: 'Input only number' }
     validates :user_id, :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :city
-    validates :address
-    validates :phone, format: { with: /\A[0-9]+\z/ }
   end
-  validates :state_id, numericality: { other_than: 1 }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
