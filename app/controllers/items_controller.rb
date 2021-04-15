5e874b494a7878907060515a9302bcfb59e548e8
makikoma <sweetness91116@gmail.com>
2021-04-15 21:40:48 +0900

    編集画面へ遷移できずトップページへ遷移する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,6 +52,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless @item.user_id == current_user.id
+    redirect_to root_path unless @item.user_id == current_user.id && @item.purchase.nil?
   end
 end
